bf8c01df01762e61f19e3171f2bb5e223268ff48
Akshita1905 <akshita19005@gmail.com>
2026-06-30 22:02:26 +0530

    fix: Import path module in index.js for static file serving

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
+const path = require("path");
 dotenv.config();
 
 connectDB();
